f95b6fd2b9a248cb2e13d95a074ff64a879f892a
Muhammad Alfin <67232524+doyzfin@users.noreply.github.com>
2026-01-27 11:57:48 +0700

    add feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { PlusIcon } from "@heroicons/react/24/solid";
 
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div></div>
+      <h1 className="text-brand-400">HRIS</h1>
+      <div className="flex justify-center items-center gap-4">
+        <PlusIcon className="h-5 w-5 text-center" aria-hidden="true" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>
+        Edit <code>src/App.jsx</code> and save to test HMR
       </p>
+      <p className="">Click on the Vite and React logos to learn more</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
