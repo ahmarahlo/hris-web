dc71e1651603ff79f72ee9e05a03b332ed3be4a6
ahmarahlo <ahmdrhmdni457@gmail.com>
2026-01-28 13:21:38 +0700

    penambahan alert

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { PlusIcon } from "@heroicons/react/24/solid";
+import { ClockIcon, PlusIcon } from "@heroicons/react/24/solid";
 import "./App.css";
 import { Alert, Button, Dropdown, Input, Table } from "./lib/components";
 
 function App() {
   return (
     <>
-      <div></div>
       <h1 className="text-brand-400">HRIS</h1>
       <div className="flex justify-center items-center gap-4">
         <PlusIcon className="h-5 w-5 text-center" aria-hidden="true" />
       </div>
-      <Alert />
-      <Button variant="primary" className={"bg-amber-300"}>Tutup</Button>
+      <Alert/>
+      <Button variant="clock" onClick={()=>{console.log('masuk')}}>
+        Tutup 
+        <ClockIcon className="h-5 w-5 text-center gap-4"/></Button>
       <Dropdown />
       <Input />
       <Table />
--- src/lib/components/alert/Alert.jsx
+++ src/lib/components/alert/Alert.jsx
@@ -1,3 +1,21 @@
-export function Alert() {
-  return <p>Alert</p>;
+import React from "react";
+import { Button } from "../button/Button";
+import dismissGif from "../../../assets/dismiss.gif";
+
+export function Alert({ title = "Ditolak", onClose }) {
+	return (
+		<div className="flex flex-col items-center bg-white p-10 rounded-3xl shadow-xl w-80 max-w-md mx-auto">
+			<div className="">
+				<img
+					src={dismissGif}
+					alt="Status Animasi"
+					className="h-40 w-40 object-contain"
+				/>
+			</div>
+			<h2 className="text-3xl font-bold text-black mb-10">{title}</h2>
+			<Button variant="danger" onClick={onClose}>
+				Tutup
+			</Button>
+		</div>
+	);
 }
