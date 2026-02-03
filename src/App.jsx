5faa169f14731d7feafb5357b084d83aaee2ed88
ahmarahlo <ahmdrhmdni457@gmail.com>
2026-02-03 08:52:21 +0700

    penambahan alert banner

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { ClockIcon, PlusIcon } from "@heroicons/react/24/solid";
 import { Alert, Button, Card, Dropdown, Input, Table } from "./lib/components";
 import { Layout } from "./lib/components/layout/layout.jsx";
 import "./App.css";
+import { AlertBanner } from "./lib/components/alert/alertBanner.jsx";
 
 function App() {
 	return (
@@ -20,6 +21,12 @@ function App() {
 					setAlertType(null);
 				}}
 			/>
+
+			<AlertBanner
+				variant="success"
+				message="APASIH?">
+
+			</AlertBanner>
 		</Layout>
 	);
 }
