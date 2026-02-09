0c90290f9042e88a1b8295684405c0cf41971c11
ahmarahlo <ahmdrhmdni457@gmail.com>
2026-02-09 10:29:03 +0700

    perbaikan dropdown

---
+++ src/lib/components/badge/Badge.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export function Badge() {
+	// TODO: Implement Badge component
+	return null;
+}
---
+++ src/lib/components/modal/Modal.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export function Modal() {
+	// TODO: Implement Modal component
+	return null;
+}
