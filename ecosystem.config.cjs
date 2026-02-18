a8ae9d7f0ddaf3fc4718b38664f0657f0651ce1e
Muhammad Alfin <67232524+doyzfin@users.noreply.github.com>
2026-02-18 10:36:07 +0700

    .

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "hris-web-app",
       script: "serve",
-      args: "-s dist -l 3007",
+      args: ["-s", "dist", "-l", "3007"],
       instances: 1,
       autorestart: true,
       watch: false,
