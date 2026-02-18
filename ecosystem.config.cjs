768fe0262ee9c09263a345351c1cefa70f800925
Muhammad Alfin <67232524+doyzfin@users.noreply.github.com>
2026-02-18 10:43:05 +0700

    .

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,10 +2,10 @@ module.exports = {
   apps: [
     {
       name: "hris-web-app",
-      script: "serve",
+      script: "./node_modules/serve/build/main.js",
       args: ["-s", "dist", "-l", "3007"],
-      instances: 1, // WAJIB 1
-      exec_mode: "fork", // jangan cluster
+      instances: 1,
+      exec_mode: "fork",
       autorestart: true,
       watch: false,
       env: {
