52b80bb785c78b067ffe6f6074d673eec6c184b2
Muhammad Alfin <67232524+doyzfin@users.noreply.github.com>
2026-02-18 10:21:50 +0700

    Create ecosystem.config.cjs

---
+++ ecosystem.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "hris-web-app",
+      script: "serve",
+      args: "-s dist -l 3007",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "200M",
+      env: {
+        NODE_ENV: "development",
+      },
+    },
+  ],
+};
